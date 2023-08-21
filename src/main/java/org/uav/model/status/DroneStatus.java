501803d1d72d6cf0f3b68c28f22dc5fb3232c7c5
Igor Faliszewski <igor.faliszewski1@gmail.com>
2023-08-21 18:04:21 +0200

    Migrate drone rotation from euler angles to quaternions

--- src/main/java/org/uav/model/status/DroneStatus.java
+++ src/main/java/org/uav/model/status/DroneStatus.java
@@ -1,5 +1,6 @@
 package org.uav.model.status;
 
+import org.joml.Quaternionf;
 import org.joml.Vector3f;
 
 import java.util.Vector;
@@ -8,7 +9,7 @@ public class DroneStatus {
     public int id;
     public float time;
     public Vector3f position;
-    public Vector3f rotation;
+    public Quaternionf rotation;
     public Vector3f linearVelocity;
     public Vector3f angularVelocity;
     public Vector<Float> propellers;
@@ -16,7 +17,7 @@ public class DroneStatus {
     public DroneStatus() {
         id = 0;
         position = new Vector3f();
-        rotation = new Vector3f();
+        rotation = new Quaternionf();
         linearVelocity = new Vector3f();
         angularVelocity = new Vector3f();
         propellers = new Vector<>();
